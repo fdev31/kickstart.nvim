cd63622ae9ecb2bb36a180c0bb3c7122020eeb53
fdev31 <fdev31@gmail.com>
2024-05-22 17:48:30 +0200

    re-indent & add couple of flags

--- lua/custom/lib.lua
+++ lua/custom/lib.lua
@@ -1,26 +1,29 @@
 local exported = {}
 
 local function file_exists(name)
-	local f = io.open(name, "r")
-	if f ~= nil then
-		io.close(f)
-		return true
-	else
-		return false
-	end
+  local f = io.open(name, 'r')
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
 end
 
 exported.has = function(tbl, item)
-	for _, v in ipairs(tbl) do
-		if v == item then
-			return true
-		end
-	end
-	return false
+  for _, v in ipairs(tbl) do
+    if v == item then
+      return true
+    end
+  end
+  return false
 end
 
-exported.isWorkLaptop = file_exists("/home/fab/liberty/code")
+exported.isWorkLaptop = file_exists '/home/fab/liberty/code'
 
-exported.isGitMergetool = vim.env.TEXTDOMAIN == "git" or vim.env.GIT_PREFIX ~= nil
+exported.isGitMergetool = vim.env.TEXTDOMAIN == 'git' or vim.env.GIT_PREFIX ~= nil
+
+exported.useCopilot = true
+exported.useCodeium = true
 
 return exported
