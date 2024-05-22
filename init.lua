38af479ea12999d8fcf2b0fd21b1640b176a3940
fdev31 <fdev31@gmail.com>
2024-05-22 23:16:06 +0200

    map "fr" to "find reference" instead

--- init.lua
+++ init.lua
@@ -429,7 +429,7 @@ local plugins = {
       vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
       vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
-      vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
+      vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = '[F]ind [R]eference' })
       vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[' .. vim.g.mapleader .. '] Find existing buffers' })
 
