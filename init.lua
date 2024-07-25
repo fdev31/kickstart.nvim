0e883279889a885efdf1b49db48790bcc36609f7
fdev31 <fdev31@gmail.com>
2024-07-25 15:38:55 +0200

    update which-key syntax

--- init.lua
+++ init.lua
@@ -288,19 +288,15 @@ local plugins = {
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[F]find', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>c', desc = '[C]ode' },
+        { '<leader>d', desc = '[D]ocument' },
+        { '<leader>h', desc = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>r', desc = '[R]ename' },
+        { '<leader>s', desc = '[F]find' },
+        { '<leader>t', desc = '[T]oggle' },
+        { '<leader>w', desc = '[W]orkspace' },
       }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 
