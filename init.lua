4638cc9936271840f859ffcbdd145fbc2b1616fc
fdev31 <fdev31@gmail.com>
2024-09-16 17:41:37 +0200

    fix treesitter init

--- init.lua
+++ init.lua
@@ -994,44 +994,6 @@ local plugins = {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    opts = {
-      ensure_installed = {
-        'bash',
-        'diff',
-        'toml',
-        'vue',
-        'c',
-        'cpp',
-        'css',
-        'python',
-        'html',
-        'javascript',
-        'json',
-        'lua',
-        'markdown',
-        'markdown_inline',
-        'vim',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<CR>',
-          scope_incremental = '<CR>',
-          node_incremental = '<TAB>',
-          node_decremental = '<S-TAB>',
-        },
-      },
-    },
   },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -124,3 +124,45 @@ vim.diagnostic.config {
 require('lspconfig.ui.windows').default_options = {
   border = _border,
 }
+
+require('nvim-treesitter.configs').setup {
+  modules = { 'highlight', 'incremental_selection', 'folding' },
+  sync_install = true,
+  ignore_install = {},
+  ensure_installed = {
+    'bash',
+    'diff',
+    'toml',
+    'vue',
+    'c',
+    'cpp',
+    'css',
+    'python',
+    'html',
+    'javascript',
+    'json',
+    'lua',
+    'markdown',
+    'markdown_inline',
+    'vim',
+  },
+  -- Autoinstall languages that are not installed
+  auto_install = true,
+  highlight = {
+    enable = true,
+    -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+    --  If you are experiencing weird indenting issues, add the language to
+    --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+    additional_vim_regex_highlighting = { 'ruby' },
+  },
+  indent = { enable = true, disable = { 'ruby' } },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<CR>',
+      scope_incremental = '<CR>',
+      node_incremental = '<TAB>',
+      node_decremental = '<S-TAB>',
+    },
+  },
+}
