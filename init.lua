d1ea30ada96b3fa8f0c1f9fc524edf7a303499e5
fdev31 <fdev31@gmail.com>
2024-09-25 16:06:33 +0200

    avoid comment clash

--- init.lua
+++ init.lua
@@ -244,7 +244,7 @@ local plugins = {
   --    require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  -- { 'numToStr/Comment.nvim', opts = {} },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
