a3feca1e53763172374438b63105647fe2921bd1
fdev31 <fdev31@gmail.com>
2024-10-06 12:32:26 +0200

    update which-key

--- init.lua
+++ init.lua
@@ -283,7 +283,7 @@ local plugins = {
 
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
       require('which-key').setup {
         win = { border = 'rounded' },
