314db29bc111768dcd18dea724d2292acba4fd4f
fdev31 <fdev31@gmail.com>
2024-10-10 23:24:52 +0200

    switch to custom confluence parser

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -92,7 +92,7 @@ autocmd({ 'BufNewFile', 'BufRead' }, {
   pattern = '*.wiki',
   callback = function()
     set_spell()
-    vim.bo.filetype = 'confluencewiki'
+    vim.bo.filetype = 'confluence_wiki'
     vim.bo.makeprg = 'br updes %'
   end,
 })
@@ -165,4 +165,13 @@ require('nvim-treesitter.configs').setup {
       node_decremental = '<S-TAB>',
     },
   },
+}
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+parser_config.confluence_wiki = {
+  install_info = {
+    url = 'https://github.com/fdev31/tree-sitter-confluence',
+    files = { 'src/parser.c' },
+    branch = 'main',
+  },
+  filetype = 'confluence_wiki', -- if you want to set the filetype automatically
 }
