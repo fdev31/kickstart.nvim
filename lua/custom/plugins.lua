283bd44bb61c9f02bfe47f6540c5c8f42d8496d5
fdev31 <fdev31@gmail.com>
2024-11-18 09:53:31 +0100

    changed markdown again

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -7,6 +7,15 @@ local settings = require 'custom.settings'
 
 M = {
   {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
     'fei6409/log-highlight.nvim',
     config = function()
       require('log-highlight').setup {
