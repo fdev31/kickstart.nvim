d2c72234cac3b9c2685c37850ee84a6ce5029e6e
fdev31 <fdev31@gmail.com>
2025-04-06 22:26:28 +0200

    shorten caption

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -17,7 +17,7 @@ M = {
       { 'nvim-tree/nvim-web-devicons', lazy = true },
     },
     keys = {
-      { '<leader>m', '<cmd>Grapple toggle<cr>', desc = 'Grapple toggle [m]ark' },
+      { '<leader>m', '<cmd>Grapple toggle<cr>', desc = 'Grapple' },
       { '<leader>M', '<cmd>Grapple toggle_tags<cr>', desc = 'Grapple [M]ove' },
       { '<leader>n', '<cmd>Grapple cycle_tags next<cr>', desc = 'Grapple cycle [n]ext tag' },
       { '<leader>p', '<cmd>Grapple cycle_tags prev<cr>', desc = 'Grapple cycle [p]revious tag' },
