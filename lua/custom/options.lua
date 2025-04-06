7d58ff0f44da180d2b4e4fe62a516d0ae3f8eab7
fdev31 <fdev31@gmail.com>
2025-04-06 15:46:41 +0200

    bind hyprlang to hyprland config files

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -161,6 +161,21 @@ parser_config.confluence_wiki = {
   filetype = 'confluence_wiki', -- if you want to set the filetype automatically
 }
 
+-- Hyprlang syntax & LSP {{{
+-- LspInstall hyprlang
+-- go install github.com/hyprland-community/hyprls/cmd/hyprls@latest
 vim.filetype.add {
   pattern = { ['.*/hypr/.*%.conf'] = 'hyprlang' },
 }
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+  pattern = { '*.hl', 'hypr*.conf' },
+  callback = function(event)
+    print(string.format('starting hyprls for %s', vim.inspect(event)))
+    vim.lsp.start {
+      name = 'hyprlang',
+      cmd = { 'hyprls' },
+      root_dir = vim.fn.getcwd(),
+    }
+  end,
+})
+-- }}}
