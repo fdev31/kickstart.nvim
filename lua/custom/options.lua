2ee4640afb0e413f34d8b0df5d59d88256f911f7
fdev31 <fdev31@gmail.com>
2025-04-06 18:44:21 +0200

    remove annoying print

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -170,7 +170,6 @@ vim.filetype.add {
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
   pattern = { '*.hl', 'hypr*.conf' },
   callback = function(event)
-    print(string.format('starting hyprls for %s', vim.inspect(event)))
     vim.lsp.start {
       name = 'hyprlang',
       cmd = { 'hyprls' },
