fc52a67d96b0d3f410fd22f18cfc8e19666ab4b2
fdev31 <fdev31@gmail.com>
2025-04-15 09:27:47 +0200

    remove rainbow

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -406,33 +406,6 @@ M = {
       require('dapui').setup()
     end,
   },
-  {
-    'HiPhish/rainbow-delimiters.nvim',
-    lazy = false,
-    config = function() -- {{{
-      local rainbow_delimiters = require 'rainbow-delimiters'
-
-      vim.g.rainbow_delimiters = {
-        strategy = {
-          [''] = rainbow_delimiters.strategy['global'],
-          vim = rainbow_delimiters.strategy['local'],
-        },
-        query = {
-          [''] = 'rainbow-delimiters',
-          lua = 'rainbow-blocks',
-        },
-        highlight = {
-          'RainbowDelimiterRed',
-          'RainbowDelimiterYellow',
-          'RainbowDelimiterBlue',
-          'RainbowDelimiterOrange',
-          'RainbowDelimiterGreen',
-          'RainbowDelimiterViolet',
-          'RainbowDelimiterCyan',
-        },
-      } -- }}}
-    end,
-  },
   { 'onsails/lspkind.nvim' },
 }
 
