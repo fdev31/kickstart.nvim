7be19d31dea1a2f6a0cf31ace56644a0c5a599a6
fdev31 <fdev31@gmail.com>
2025-04-22 19:23:42 +0200

    fix overseer not starting

--- lua/custom/plugins/menus.lua
+++ lua/custom/plugins/menus.lua
@@ -1,4 +1,7 @@
 return {
   { 'fdev31/menus.nvim' }, -- menus
-  { 'stevearc/overseer.nvim' }, -- detect runnables, used in menus.nvim config
+  {
+    'stevearc/overseer.nvim',
+    opts = {},
+  }, -- detect runnables, used in menus.nvim config
 }
