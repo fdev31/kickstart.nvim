f9e21cd47a60aba37d3d07469494de9ab3a2274a
fdev31 <fdev31@gmail.com>
2025-04-30 20:32:08 +0200

    restore highlight on hover

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -48,6 +48,32 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Rename the variable under your cursor.
     --  Most Language Servers support renaming across files, etc.
     lspmap('<leader>rn', vim.lsp.buf.rename, 'Re[n]ame')
+
+    -- hover
+
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client then
+      local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.clear_references,
+      })
+
+      vim.api.nvim_create_autocmd('LspDetach', {
+        group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+        callback = function(event2)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+        end,
+      })
+    end
   end,
 })
 
