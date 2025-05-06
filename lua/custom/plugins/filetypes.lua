2447be655162c9daf822003792b0b756962e83d7
fdev31 <fdev31@gmail.com>
2025-05-06 16:45:20 +0200

    different UML integration

--- lua/custom/plugins/filetypes.lua
+++ lua/custom/plugins/filetypes.lua
@@ -7,6 +7,30 @@ return {
       vim.api.nvim_set_hl(0, 'PlantumlColonLine', { link = '@character' })
     end,
   },
+  {
+    'https://gitlab.com/itaranto/plantuml.nvim',
+    version = '*',
+    config = function()
+      require('plantuml').setup {
+        renderer = {
+          type = 'imv',
+          options = {
+            dark_mode = true,
+            format = nil, -- Allowed values: nil, 'png', 'svg'.
+          },
+        },
+        render_on_write = true,
+      }
+    end,
+  },
+  -- {
+  --   'https://gitlab.com/itaranto/preview.nvim',
+  --   opts = {},
+  --   config = function()
+  --     vim.opt.spell = false
+  --     -- vim.api.nvim_set_hl(0, 'PlantumlColonLine', { link = '@character' })
+  --   end,
+  -- },
   { 'NoahTheDuke/vim-just', ft = 'just', config = function() end },
   -- { 'nvim-neotest/nvim-nio' },
 }
