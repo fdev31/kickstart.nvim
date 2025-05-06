372b7c09804d33847b6f937fadb5baf8f75684d2
fdev31 <fdev31@gmail.com>
2025-05-06 13:53:29 +0200

    better plantuml

--- lua/custom/plugins/filetypes.lua
+++ lua/custom/plugins/filetypes.lua
@@ -1,5 +1,12 @@
 return {
-  { 'aklt/plantuml-syntax', ft = 'plantuml' },
+  {
+    'aklt/plantuml-syntax',
+    ft = 'plantuml',
+    config = function()
+      vim.opt.spell = false
+      vim.api.nvim_set_hl(0, 'PlantumlColonLine', { link = '@character' })
+    end,
+  },
   { 'NoahTheDuke/vim-just', ft = 'just', config = function() end },
   -- { 'nvim-neotest/nvim-nio' },
 }
