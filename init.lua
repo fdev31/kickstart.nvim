cead45c297cc024a1ab9bcbb55a12a8060d17299
fdev31 <fdev31@gmail.com>
2025-05-22 20:54:27 +0200

    Install automatically

--- init.lua
+++ init.lua
@@ -611,7 +611,7 @@ require('lazy').setup({
 
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_installation = false,
+        automatic_installation = true,
         automatic_enable = false,
       }
 
