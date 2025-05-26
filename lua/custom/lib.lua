c53d82908013f20348bbc64fcab7d26140f772e5
fdev31 <fdev31@gmail.com>
2025-05-26 13:32:43 +0200

    fix file_exists usage

--- lua/custom/lib.lua
+++ lua/custom/lib.lua
@@ -1,4 +1,4 @@
-return {
+local M = {
   floating_win_exists = function()
     for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
       if vim.api.nvim_win_get_config(winid).zindex then
@@ -61,25 +61,6 @@ return {
     end
     return false
   end,
-  openUnder = function()
-    -- Try LSP definition
-    local result = vim.lsp.buf_request_sync(0, 'textDocument/definition', vim.lsp.util.make_position_params(), 500)
-
-    if result and next(result) then
-      for _, res in pairs(result) do
-        if res.result and #res.result > 0 then
-          return vim.lsp.buf.definition()
-        end
-      end
-    end
-    local file_under_cursor = vim.fn.fnamemodify(vim.fn.expand '<cfile>', ':p')
-    -- Check if the file exists and is readable
-    if vim.fn.filereadable(file_under_cursor) or vim.fn.isdirectory(file_under_cursor) then
-      vim.cmd('edit ' .. vim.fn.fnameescape(file_under_cursor))
-    else
-      vim.notify('Could not find ' .. file_under_cursor, vim.log.levels.WARN)
-    end
-  end,
   isGitMergetool = vim.env.TEXTDOMAIN == 'git' or vim.env.GIT_PREFIX ~= nil,
 
   filter_prop = function(list, propname, not_value)
@@ -102,3 +83,23 @@ return {
     end
   end,
 }
+M.openUnder = function()
+  -- Try LSP definition
+  local result = vim.lsp.buf_request_sync(0, 'textDocument/definition', vim.lsp.util.make_position_params(), 500)
+
+  if result and next(result) then
+    for _, res in pairs(result) do
+      if res.result and #res.result > 0 then
+        return vim.lsp.buf.definition()
+      end
+    end
+  end
+  local file_under_cursor = vim.fn.fnamemodify(vim.fn.expand '<cfile>', ':p')
+  -- Check if the file exists and is readable
+  if M.file_exists(file_under_cursor) then
+    vim.cmd('edit ' .. vim.fn.fnameescape(file_under_cursor))
+  else
+    vim.notify('Could not find ' .. file_under_cursor, vim.log.levels.WARN)
+  end
+end
+return M
