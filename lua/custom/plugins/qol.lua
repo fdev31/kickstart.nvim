516fd81879be0d9f1dd715d9f0f89fa42f704948
fdev31 <fdev31@gmail.com>
2025-06-10 10:38:42 +0200

    add grapple

--- lua/custom/plugins/qol.lua
+++ lua/custom/plugins/qol.lua
@@ -2,6 +2,22 @@ local lib = require 'custom.lib'
 
 return {
   {
+    'cbochs/grapple.nvim',
+    opts = {
+      scope = 'git_branch', -- also try out "git"
+      icons = false,
+      status = false,
+    },
+    event = { 'BufReadPost', 'BufNewFile' },
+    cmd = 'Grapple',
+    keys = {
+      { '<leader>m', '<cmd>Grapple toggle<cr>', desc = 'Grapple toggle tag' },
+      { '<leader>M', '<cmd>Grapple toggle_tags<cr>', desc = 'Grapple open tags window' },
+      { '<leader>n', '<cmd>Grapple cycle_tags next<cr>', desc = 'Grapple cycle next tag' },
+      { '<leader>p', '<cmd>Grapple cycle_tags prev<cr>', desc = 'Grapple cycle previous tag' },
+    },
+  },
+  {
     'chentoast/marks.nvim',
     event = 'VeryLazy',
     config = function()
