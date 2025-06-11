cb41079c4606319a02399d2b5cbbbff87825af9f
fdev31 <fdev31@gmail.com>
2025-06-11 20:00:26 +0200

    cleanup treesitter opts

--- lua/custom/settings.lua
+++ lua/custom/settings.lua
@@ -103,6 +103,47 @@ local M = {
       },
     },
   },
+  treesitter_opts = {
+    modules = { 'highlight', 'incremental_selection', 'folding', 'mashup' },
+    sync_install = true,
+    ignore_install = {},
+    ensure_installed = {
+      'bash',
+      'diff',
+      'toml',
+      'vue',
+      'c',
+      'cpp',
+      'css',
+      'python',
+      'html',
+      'javascript',
+      'json',
+      'lua',
+      'markdown',
+      'markdown_inline',
+      'vim',
+    },
+    -- Autoinstall languages that are not installed
+    auto_install = true,
+    highlight = {
+      enable = true,
+      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+      --  If you are experiencing weird indenting issues, add the language to
+      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+      additional_vim_regex_highlighting = { 'ruby' },
+    },
+    indent = { enable = true, disable = { 'ruby' } },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<CR>',
+        scope_incremental = '<CR>',
+        node_incremental = '<TAB>',
+        node_decremental = '<S-TAB>',
+      },
+    },
+  },
   lsp_servers = {
     textlsp = {},
     harper_ls = {
