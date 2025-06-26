1ede882dad7ba76c987585adc50b58e164293449
fdev31 <fdev31@gmail.com>
2025-06-27 00:43:24 +0200

    add flash

--- lua/custom/plugins/qol.lua
+++ lua/custom/plugins/qol.lua
@@ -1,6 +1,17 @@
 local lib = require 'custom.lib'
 
 return {
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    },
+  },
   { 'windwp/nvim-ts-autotag' },
   { 'windwp/nvim-autopairs' },
   {
