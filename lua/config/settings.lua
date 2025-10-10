fad3a7a2734438711650b8794134cf7e1e6991b6
fdev31 <fdev31@gmail.com>
2025-10-10 20:54:05 +0200

    move vue definition

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -88,14 +88,6 @@ vim.schedule(function()
   vim.o.clipboard = 'unnamedplus'
 end)
 
-local vue_language_server_path = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server'
-local vue_plugin = {
-  name = '@vue/typescript-plugin',
-  location = vue_language_server_path,
-  languages = { 'vue' },
-  configNamespace = 'typescript',
-}
-
 return {
   leader = '²',
   diagnostic_config = {
