064507eef606c732eaaa288e29aebc6ab21c1562
fdev31 <fdev31@gmail.com>
2025-10-22 19:57:13 +0200

    small refactor

--- lua/config/late_options.lua
+++ lua/config/late_options.lua
@@ -1,57 +1,53 @@
 -- vim:ts=2:sw=2:et:
+
+vim.o.clipboard = 'unnamedplus'
+
+-- Go to the file's folder with :Chdir
+vim.api.nvim_create_user_command('Chdir', 'cd %:h', {})
+
+-- Load custom snippets
+require('luasnip.loaders.from_vscode').lazy_load { paths = { '~/.config/Code/User/snippets/' } }
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
 local autocmd = vim.api.nvim_create_autocmd
---
--- delayed to save some startup time
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+-- update layouts when resizing
+autocmd('VimResized', {
+  pattern = '*',
+  command = 'wincmd =',
+})
 
-  -- Go to the file's folder with :Chdir
-  vim.api.nvim_create_user_command('Chdir', 'cd %:h', {})
+if vim.g.neovide then
+  local mapKey = vim.keymap.set
+  mapKey('!', '<S-Insert>', '<C-R>+') -- allow Shit+Insert on the prompt
 
-  -- Load custom snippets
-  require('luasnip.loaders.from_vscode').lazy_load { paths = { '~/.config/Code/User/snippets/' } }
-
-  -- [[ Basic Autocommands ]]
-  --  See `:help lua-guide-autocommands`
-
-  -- Highlight when yanking (copying) text
-  --  Try it with `yap` in normal mode
-  --  See `:help vim.hl.on_yank()`
-  autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  vim.g.neovide_opacity = 0.90
+  vim.g.neovide_cursor_vfx_mode = 'railgun'
+  -- vim.g.neovide_scale_factor = 0.8
+  -- Dynamic Scale
+  local _scaleChange = function(fac)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * fac
+  end
+  mapKey('n', '<C-=>', '', {
+    silent = true,
     callback = function()
-      vim.hl.on_yank()
+      _scaleChange(1.2)
     end,
   })
-  -- update layouts when resizing
-  autocmd('VimResized', {
-    pattern = '*',
-    command = 'wincmd =',
+  mapKey('n', '<C-->', '', {
+    silent = true,
+    callback = function()
+      _scaleChange(1 / 1.2)
+    end,
   })
-
-  if vim.g.neovide then
-    local mapKey = vim.keymap.set
-    mapKey('!', '<S-Insert>', '<C-R>+') -- allow Shit+Insert on the prompt
-
-    vim.g.neovide_opacity = 0.90
-    vim.g.neovide_cursor_vfx_mode = 'railgun'
-    -- vim.g.neovide_scale_factor = 0.8
-    -- Dynamic Scale
-    local _scaleChange = function(fac)
-      vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * fac
-    end
-    mapKey('n', '<C-=>', '', {
-      silent = true,
-      callback = function()
-        _scaleChange(1.2)
-      end,
-    })
-    mapKey('n', '<C-->', '', {
-      silent = true,
-      callback = function()
-        _scaleChange(1 / 1.2)
-      end,
-    })
-  end
-end)
+end
