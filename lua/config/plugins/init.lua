08741922570ad7876f76b00714880c6dcdd25512
fdev31 <fdev31@gmail.com>
2025-10-23 23:55:22 +0200

    plugins are now a setting

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -1,38 +1,8 @@
--- You can add your own plugins here or in other files in this directory!:
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 M = {}
 
 local async_setup = true
 
-local plugins = {
-  'autocomplete',
-  'autoformat',
-  'code_goodies',
-  'copilot',
-  'daps',
-  'diagnostics',
-  'diffview',
-  'embedded',
-  'filetypes',
-  'git',
-  'http_client',
-  'lsp',
-  'markdown',
-  'menus',
-  'misc',
-  'neotree',
-  'orgmode',
-  'python',
-  'qol',
-  'silicon',
-  'smooth_scroll',
-  'theme',
-  'treesitter',
-  'whichkey',
-  'workspaces',
-}
+local settings = require 'config.settings'
 
 local load_plugin = function(plugin)
   local plug = require('config.plugins.' .. plugin)
@@ -48,7 +18,7 @@ local load_plugin = function(plugin)
   end
 end
 
-for _, plugin in ipairs(plugins) do
+for _, plugin in ipairs(settings.plugins) do
   local ret, msg = pcall(load_plugin, plugin)
   if not ret then
     vim.notify('Failed to load ' .. plugin .. ': ' .. msg, vim.log.levels.ERROR, { title = 'Plugin loader' })
