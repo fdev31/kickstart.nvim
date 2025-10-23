dc57be1833f2f8661c0ca2c6f822d12d03bb11e5
fdev31 <fdev31@gmail.com>
2025-10-24 00:48:19 +0200

    change iconset

--- lua/config/plugins/markdown.lua
+++ lua/config/plugins/markdown.lua
@@ -14,9 +14,9 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim', -- better markdown
     ft = { 'markdown', 'codecompanion' },
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
   },
