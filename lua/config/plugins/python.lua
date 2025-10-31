3130ac79496edd2a8d0820455824fe416cb6af76
fdev31 <fdev31@gmail.com>
2025-10-31 12:46:09 +0100

    update branch

--- lua/config/plugins/python.lua
+++ lua/config/plugins/python.lua
@@ -13,7 +13,7 @@ return {
     'linux-cultist/venv-selector.nvim',
     ft = { 'python' },
     lazy = true,
-    branch = 'regexp',
+    branch = 'main',
     dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
     opts = {
       -- Your options go here
