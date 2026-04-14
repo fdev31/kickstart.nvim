c2c3542e757c68aeb64d8181e43cd7c27f18c23c
fdev31 <fdev31@gmail.com>
2026-04-14 21:56:23 +0200

    fix: skip incremental selection keymaps in special buffers
    Use expr mappings that check buftype and fall back to default key
    behavior in help, quickfix, terminal, and other special buffers.

--- plugin/04-treesitter.lua
+++ plugin/04-treesitter.lua
@@ -29,9 +29,28 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
--- Incremental selection keymaps
-vim.keymap.set('n', '<CR>', 'van', { remap = true, desc = 'Start incremental selection' })
-vim.keymap.set('v', '<CR>', 'an', { remap = true, desc = 'Expand selection to parent node' })
-vim.keymap.set('v', '<S-CR>', 'in', { remap = true, desc = 'Shrink selection to child node' })
-vim.keymap.set('v', '<Tab>', ']n', { remap = true, desc = 'Select next sibling node' })
-vim.keymap.set('v', '<S-Tab>', '[n', { remap = true, desc = 'Select previous sibling node' })
+-- Incremental selection keymaps (skip special buffers: help, quickfix, terminal, etc.)
+vim.keymap.set('n', '<CR>', function()
+  if vim.bo.buftype ~= '' then return '<CR>' end
+  return 'van'
+end, { remap = true, expr = true, desc = 'Start incremental selection' })
+
+vim.keymap.set('v', '<CR>', function()
+  if vim.bo.buftype ~= '' then return '<CR>' end
+  return 'an'
+end, { remap = true, expr = true, desc = 'Expand selection to parent node' })
+
+vim.keymap.set('v', '<S-CR>', function()
+  if vim.bo.buftype ~= '' then return '<S-CR>' end
+  return 'in'
+end, { remap = true, expr = true, desc = 'Shrink selection to child node' })
+
+vim.keymap.set('v', '<Tab>', function()
+  if vim.bo.buftype ~= '' then return '<Tab>' end
+  return ']n'
+end, { remap = true, expr = true, desc = 'Select next sibling node' })
+
+vim.keymap.set('v', '<S-Tab>', function()
+  if vim.bo.buftype ~= '' then return '<S-Tab>' end
+  return '[n'
+end, { remap = true, expr = true, desc = 'Select previous sibling node' })
