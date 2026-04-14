167b5b6b1941bba9ec648d1a1fd38a6d1f2e7f8f
fdev31 <fdev31@gmail.com>
2026-04-15 01:00:51 +0200

    refactor

--- init.lua
+++ init.lua
@@ -18,25 +18,26 @@ vim.api.nvim_create_autocmd('PackChanged', {
   group = augroup,
   callback = function(ev)
     local name, kind = ev.data.spec.name, ev.data.kind
-    -- Rebuild treesitter parsers on install/update
-    if name == 'nvim-treesitter' and (kind == 'install' or kind == 'update') then
-      if not ev.data.active then
-        vim.cmd.packadd('nvim-treesitter')
-      end
-      vim.cmd('TSUpdate')
-    end
-    -- Rebuild telescope-fzf-native on install/update
-    if name == 'telescope-fzf-native.nvim' and (kind == 'install' or kind == 'update') then
-      if vim.fn.executable('make') == 1 then
-        local path = vim.fn.stdpath('data') .. '/site/pack/core/opt/telescope-fzf-native.nvim'
-        vim.fn.system({ 'make', '-C', path })
-      end
-    end
-    -- Rebuild LuaSnip jsregexp on install/update
-    if name == 'LuaSnip' and (kind == 'install' or kind == 'update') then
-      if vim.fn.executable('make') == 1 then
-        local path = vim.fn.stdpath('data') .. '/site/pack/core/opt/LuaSnip'
-        vim.fn.system({ 'make', 'install_jsregexp', '-C', path })
+    -- Handle install & updates
+    if kind == 'install' or kind == 'update' then
+      -- Rebuild treesitter parsers on install/update
+      if name == 'nvim-treesitter' then
+        if not ev.data.active then
+          vim.cmd.packadd 'nvim-treesitter'
+        end
+        vim.cmd 'TSUpdate'
+      elseif name == 'telescope-fzf-native.nvim' then
+        -- Rebuild telescope-fzf-native on install/update
+        if vim.fn.executable 'make' == 1 then
+          local path = vim.fn.stdpath 'data' .. '/site/pack/core/opt/telescope-fzf-native.nvim'
+          vim.fn.system { 'make', '-C', path }
+        end
+      elseif name == 'LuaSnip' then
+        -- Rebuild LuaSnip jsregexp on install/update
+        if vim.fn.executable 'make' == 1 then
+          local path = vim.fn.stdpath 'data' .. '/site/pack/core/opt/LuaSnip'
+          vim.fn.system { 'make', 'install_jsregexp', '-C', path }
+        end
       end
     end
   end,
