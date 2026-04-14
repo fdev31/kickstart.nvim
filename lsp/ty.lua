11623c6e259330c5affb38523387d15a1401b1ef
fdev31 <fdev31@gmail.com>
2026-04-14 23:55:56 +0200

    fix: disable ty semantic tokens to prevent imported functions showing as variables
    ty v0.0.30 correctly identifies function definitions but misclassifies
    imported and used functions as 'variable' semantic token type, overriding
    treesitter's correct highlighting.

--- lsp/ty.lua
+++ lsp/ty.lua
@@ -1,1 +1,7 @@
-return {}
+return {
+  on_attach = function(client)
+    -- ty v0.0.30 misclassifies imported/used functions as "variable" in semantic tokens;
+    -- disable until upstream fixes this, let treesitter handle highlighting instead
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+}
