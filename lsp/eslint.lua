3d27475c2f74c63db70a3fee490cebc86bfc1ba8
fdev31 <fdev31@gmail.com>
2026-04-15 22:38:47 +0200

    Silently stop ESLint LSP when no library is found in the project

--- lsp/eslint.lua
+++ lsp/eslint.lua
@@ -1,1 +1,10 @@
-return {}
+return {
+  handlers = {
+    -- Silently stop the client when no ESLint library is found in the project,
+    -- instead of showing a warning on every buffer open.
+    ['eslint/noLibrary'] = function(_, _, ctx)
+      vim.lsp.stop_client(ctx.client_id, true)
+      return {}
+    end,
+  },
+}
