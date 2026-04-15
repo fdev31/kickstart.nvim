ef90af9f5651393e7a0802463726044ef3c1fd68
fdev31 <fdev31@gmail.com>
2026-04-15 09:58:34 +0200

    Update the colorizer plugin

--- plugin/17-qol.lua
+++ plugin/17-qol.lua
@@ -1,15 +1,15 @@
 -- vim:ts=2:sw=2:et:
 -- SCHEDULE: quality of life plugins
 vim.schedule(function()
-  vim.pack.add({
+  vim.pack.add {
     'https://github.com/NMAC427/guess-indent.nvim',
     'https://github.com/chentoast/marks.nvim',
     'https://github.com/kkharji/sqlite.lua',
     'https://github.com/nvim-telescope/telescope-fzy-native.nvim',
     'https://github.com/danielfalk/smart-open.nvim',
-    'https://github.com/norcalli/nvim-colorizer.lua',
+    'https://github.com/catgoose/nvim-colorizer.lua',
     'https://github.com/klen/nvim-config-local',
-  })
+  }
 
   require('marks').setup()
   vim.api.nvim_set_hl(0, 'MarkSignHL', { link = 'AerialLine' })
@@ -21,12 +21,12 @@ vim.schedule(function()
     RRGGBBAA = true,
   })
 
-  require('config-local').setup({
+  require('config-local').setup {
     config_files = { '.nvim.lua', '.nvimrc', '.exrc' },
-    hashfile = vim.fn.stdpath('data') .. '/config-local',
+    hashfile = vim.fn.stdpath 'data' .. '/config-local',
     autocommands_create = true,
     commands_create = true,
     silent = false,
     lookup_parents = true,
-  })
+  }
 end)
