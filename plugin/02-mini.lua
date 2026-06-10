4f4b27ead1f24b171fe8137cdf5898c7eb5d3853
fdev31 <fdev31@gmail.com>
2026-06-10 17:05:48 +0200

    enable mini  surround

--- plugin/02-mini.lua
+++ plugin/02-mini.lua
@@ -52,6 +52,8 @@ require('mini.ai').setup {
   },
 }
 
+require('mini.surround').setup {}
+
 local statusline = require 'mini.statusline'
 statusline.setup { use_icons = vim.g.have_nerd_font }
 statusline.section_location = render_statusline
