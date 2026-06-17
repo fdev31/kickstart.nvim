7c7b2065abe16a53737ccc9014a6888f30f79659
fdev31 <fdev31@gmail.com>
2026-06-17 13:54:30 +0200

    fix kulala loading

--- plugin/45-http-client.lua
+++ plugin/45-http-client.lua
@@ -3,7 +3,7 @@
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'http', 'rest' },
   once = true,
-  callback = function()
+  callback = function(ev)
     vim.pack.add({
       'https://github.com/mistweaverco/kulala.nvim',
     })
@@ -13,5 +13,13 @@ vim.api.nvim_create_autocmd('FileType', {
       global_keymaps_prefix = '<leader>r',
       kulala_keymaps_prefix = '',
     })
+
+    -- kulala.setup() registers a FileType autocmd to attach its LSP, but that
+    -- autocmd is registered AFTER the FileType event for the current buffer
+    -- has already fired. Attach manually for this buffer so completion works
+    -- on the file that triggered the lazy-load.
+    pcall(function()
+      require('kulala.cmd.lsp').start(ev.buf, ev.match)
+    end)
   end,
 })
